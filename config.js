fa9a29f54f19559e6007fc4a1b4bd015d3104404
telitom702-cmd <telitom702@gmail.com>
2026-09-25 00:57:47 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  BOT_TOKEN: "YOUR_BOT_TOKEN_HERE",
+  BOT_TOKEN: "8982103415:AAH5meSpQewu-0nBm-yTk1-BBhLyaaOXjS4",
 
   // চাইলে আপনার Telegram user ID
-  OWNER_ID: 123456789,
+  OWNER_ID: 8248792819,
 
   // Noise reduction level
   NOISE_LEVEL: "medium"
