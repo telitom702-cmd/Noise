238da243a9a29a9b27c5b2b5820949283822c261
telitom702-cmd <telitom702@gmail.com>
2026-09-24 19:50:01 +0600

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", "0"))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "24776633"))
+    API_HASH = os.environ.get("API_HASH", "57b1f632044b4e718f5dce004a988d69")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8982103415:AAH5meSpQewu-0nBm-yTk1-BBhLyaaOXjS4")
 
     # Optional
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1004456487791"))
 
     # Maximum processing size in MB
     MAX_FILE_SIZE = int(
